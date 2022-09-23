c546422b8f07ff79953a07dd8ab9c130819e6174
natassja.lessa <natassjalessa1@gmail.com>
2022-09-23 15:25:47 -0300

    App como main

--- Biblioteca/src/main/java/br/edu/femass/model/Exemplar.java
+++ Biblioteca/src/main/java/br/edu/femass/model/Exemplar.java
@@ -1,4 +1,29 @@
 package br.edu.femass.model;
 
+import java.time.LocalDate;
+
 public class Exemplar {
+
+    private Long codigo;
+    private LocalDate dataAquisicao;
+
+    public Exemplar() {
+
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getDataAquisicao() {
+        return dataAquisicao;
+    }
+
+    public void setDataAquisicao(LocalDate dataAquisicao) {
+        this.dataAquisicao = dataAquisicao;
+    }
 }
